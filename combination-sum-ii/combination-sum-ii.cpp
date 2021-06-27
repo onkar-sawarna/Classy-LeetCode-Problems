34946335e413f950ebe734e97cd2e37cfedac8cf
ONKAR SAWARNA <onkarsawarna@gmail.com>
2021-06-27 17:07:50 +0530

    Time: 4 ms (92.86%), Space: 10.5 MB (94.10%) - LeetHub

---
+++ combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void helper(vector<int>& c, int tar,vector<vector<int>>& a,vector<int>& contri,int i)
+    {   int n=c.size();
+        //base cases
+        if(i==n ){
+            if(tar == 0)
+              a.push_back(contri);
+            return;
+        }
+        if(tar <= 0){
+            if(tar == 0)
+              a.push_back(contri);
+            return;
+        }
+        if(tar - c[i] < 0){
+            return;
+        }
+        
+        contri.push_back(c[i]);
+        helper(c,tar-c[i],a,contri,i+1);
+        contri.pop_back();
+        int j;
+        int prev = c[i];
+        for(j=i;j<n and c[j]==prev;j++);
+        helper(c,tar,a,contri,j);
+        
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> contri;
+        sort(candidates.begin(),candidates.end());
+        helper(candidates,target,ans,contri,0);
+        return ans;
+    }
+};
