ac36a7fceed33f02e6bc6b523c8c1826ddbd0ed7
ONKAR SAWARNA <onkarsawarna@gmail.com>
2021-07-03 23:18:49 +0530

    Time: 16 ms (69.38%), Space: 14.2 MB (52.66%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -13,6 +13,9 @@ public:
     if (!root || root == p || root == q) return root;
     TreeNode* left = lowestCommonAncestor(root->left, p, q);
     TreeNode* right = lowestCommonAncestor(root->right, p, q);
-    return !left ? right : !right ? left : root;
+    //return !left ? right : !right ? left : root;
+       if(!left)return right;
+       else if(!right)return left;
+       else return root;
    }
 };
