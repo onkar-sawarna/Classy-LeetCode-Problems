c7cfdc1a321fcec23ae4930eff1e98602801afd5
ONKAR SAWARNA <onkarsawarna@gmail.com>
2021-08-10 10:02:36 +0530

    Time: 4 ms (91.58%), Space: 10.7 MB (66.00%) - LeetHub

--- combination-sum-ii/combination-sum-ii.cpp
+++ combination-sum-ii/combination-sum-ii.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
-    void helper(vector<int>& c, int tar,vector<vector<int>>& a,vector<int>& contri,int i)
-    {   int n=c.size();
-        //base cases
-       
-        
-            if(tar == 0){
-             a.push_back(contri);
-            return;
+    void helper(vector<int>& candidates,int target,vector<int>& contri,vector<vector<int>>& ans,int idx){
+        if(target == 0){
+            ans.push_back(contri);
         }
-         if(i==n ){
-            return;
-        }if(tar < 0){
-            return;
+        for(int i=idx;i<candidates.size();i++){
+            if(i>idx and candidates[i]==candidates[i-1])continue;
+            // NOTE : this condition helps neglecting making identical sets
+            //  this is the catch of this question
+            if(candidates[i] > target)
+            {
+                break;
+            }
+            contri.push_back(candidates[i]);
+            helper(candidates,target-candidates[i],contri,ans,i+1);
+            contri.pop_back();
         }
-        if(tar - c[i] < 0){
-            return;
-        }
-        
-        contri.push_back(c[i]);
-        helper(c,tar-c[i],a,contri,i+1);
-        contri.pop_back();
-        int j;
-        int prev = c[i];
-        for(j=i;j<n and c[j]==prev;j++);
-        helper(c,tar,a,contri,j);
-        
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> contri;
         sort(candidates.begin(),candidates.end());
-        helper(candidates,target,ans,contri,0);
+        helper(candidates,target,contri,ans,0);
         return ans;
     }
 };
