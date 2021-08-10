c3874f581bf8ea4a9270ba7cb96fb06291f03214
ONKAR SAWARNA <onkarsawarna@gmail.com>
2021-08-10 10:11:22 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (86.36%) - LeetHub

--- subsets-ii/subsets-ii.cpp
+++ subsets-ii/subsets-ii.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    void recursion(vector<vector<int>>& ans,vector<int>& contri,vector<int>& nums,int i){
-        if(i == nums.size()){
-            ans.push_back(contri);
-            return;
+    void solve(vector<int>& nums,int idx,vector<int>& contri,vector<vector<int>>& ans){
+        //for each recursive call include contri in the answer
+        ans.push_back(contri);
+        for(int i=idx;i<nums.size();i++){
+            if(i>idx and nums[i]==nums[i-1])continue;
+            contri.push_back(nums[i]);
+            solve(nums,i+1,contri,ans);
+            contri.pop_back();
         }
-        //Exclude
-        int prev = nums[i],id;
-        for(id=i;id<nums.size() and nums[id]==prev;id++);
-        recursion(ans,contri,nums,id);
-        //Include
-        
-        contri.push_back(nums[i]);
-        recursion(ans,contri,nums,i+1);
-        contri.pop_back();
-        
+        //there is no need of any base condition it will automatically stop
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> contri;
         sort(nums.begin(),nums.end());
-        recursion(ans,contri,nums,0);
+        solve(nums,0,contri,ans);
         return ans;
     }
 };
