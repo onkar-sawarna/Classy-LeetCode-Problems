99f03ec61a490332a1a150b7e26afd3df889d573
ONKAR SAWARNA <onkarsawarna@gmail.com>
2021-08-10 10:22:01 +0530

    Time: 20 ms (86.63%), Space: 9.1 MB (68.65%) - LeetHub

--- combinations/combinations.cpp
+++ combinations/combinations.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    void helper(int n,int k,vector<vector<int>>& ans,vector<int>& contri,int i){
-        
-        if(k<=0){
-            if(k==0)
-             ans.push_back(contri);
-            return;
+    void helper(int n,int k,vector<vector<int>>& ans,vector<int>& contri,int idx){
+        if(k == 0){
+            ans.push_back(contri);
         }
-        if(i>n){
-            return;
+        for(int i=idx;i<=n;i++){
+            contri.push_back(i);
+            helper(n,k-1,ans,contri,i+1);
+            contri.pop_back();
         }
-        contri.push_back(i);
-        helper(n,k-1,ans,contri,i+1);
-        contri.pop_back();
-        helper(n,k,ans,contri,i+1);
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> ans;
+     vector<vector<int>> ans;
         vector<int> contri;
         helper(n,k,ans,contri,1);
         return ans;
