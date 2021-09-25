56797752cecfc7ae56ec985e773bfb60a9c48c04
ONKAR SAWARNA <onkarsawarna@gmail.com>
2021-09-25 09:09:37 +0530

    Time: 232 ms (93.65%), Space: 101.6 MB (40.76%) - LeetHub

---
+++ minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPossible(double speed,double hour,vector<int>& dist){
+        double ans = 0.0;
+        int n = dist.size();
+        for(int i=0;i<n-1;i++){
+            ans += ceil(dist[i]/speed);
+        }
+        ans += (double)dist.back()/speed;
+        if(ans <= hour)
+            return true;
+        return false;
+    }
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int lo,hi,n=dist.size();
+        lo = 1;
+        hi = 1e7;
+        while(lo<hi){
+            int mid = lo + (hi-lo)/2;
+            if(isPossible(mid,hour,dist)){
+                hi = mid;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+        if(isPossible(lo,hour,dist))
+            return lo;
+        else
+            return -1;
+    }
+};
